4c8338cb8752ab04f675f116fe3cf0dadabd336f
Erik Sargsyan <erosargsyan7@gmail.com>
2021-08-03 22:05:20 +0400

    fix(App.js): remove div component

--- blog_app/src/App.js
+++ blog_app/src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import PrimaryPage from "./components/primaryPage";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
       <Router>
-        <div className="App">
-          <PrimaryPage />
-        </div>
+        <PrimaryPage />
       </Router>
     </React.Fragment>
   );
