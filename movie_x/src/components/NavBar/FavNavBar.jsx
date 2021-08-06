8616b8b4b2edba77fc43d479c7c98bf59b1ae0ef
Erik Sargsyan <erosargsyan7@gmail.com>
2021-08-06 17:02:46 +0400

    feat(FavNavBar): add FavNavBar

---
+++ movie_x/src/components/NavBar/FavNavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useHistory } from "react-router";
+import { NavLink } from "react-router-dom";
+
+export default function FavNavbar() {
+  let history = useHistory();
+
+  return (
+    <div className=" flex justify-between items-center    bg-white shadow-xs w-full ">
+      <div className="bg-green-500 px-4 py-2  text-white  space-x-2 rounded  mt-8 ml-12">
+        <NavLink className="text-md font-medium ml-2 " to="/movies">
+          <button>Go back</button>
+        </NavLink>
+      </div>
+      <br />
+      <div>
+        <button
+          className="bg-red-500 px-4 py-2  text-white  space-x-2 rounded mt-8 mr-12"
+          onClick={() => {
+            localStorage.removeItem("auth");
+            history.push("/");
+          }}
+        >
+          Log out
+        </button>
+      </div>
+    </div>
+  );
+}
